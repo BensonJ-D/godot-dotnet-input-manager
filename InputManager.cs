7c0506b661b595fe3f362548ed13d2acd086b9f4
Michael Hinz <michael@tinyislandgames.com>
2026-04-10 01:40:58 +1000

    - Update imports

--- InputManager.cs
+++ InputManager.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Godot;
-using Godot.Collections;
 using DotNetInputManager.InputImageMapping;
 
 namespace DotNetInputManager;
--- InputModal.cs
+++ InputModal.cs
@@ -2,7 +2,7 @@ using Godot;
 using GodotTask;
 using Serilog;
 
-namespace ProjectKale.UI;
+namespace DotNetInputManager;
 
 public partial class InputModal : Popup
 {
